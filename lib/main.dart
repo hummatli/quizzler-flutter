f2230355aad37bb3dce50a94a370afe6ad4e1919
Sattar Hummatli <shummatli@Sattars-MacBook-Pro.local>
2020-02-20 11:28:48 +0400

    Add status keeper list - 2

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,13 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int currentQuestIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[currentQuestIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,6 +70,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  currentQuestIndex++;
                   scoreKeeper.add(Icon(
                     Icons.check,
                     color: Colors.green,
